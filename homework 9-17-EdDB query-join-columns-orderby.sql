4cecbb47560dee54b16ed09ad16671f45789b67c
Eileen Byrne <byrne.me@gmail.com>
2020-09-17 09:47:27 -0400

    with right answer

--- homework 9-17-EdDB query-join-columns-orderby.sql
+++ homework 9-17-EdDB query-join-columns-orderby.sql
@@ -1,7 +1,11 @@
-select s.id, description 'degree', minsat, firstname, lastname, sat, gpa
+/*select s.id, description 'degree', minsat, firstname, lastname, sat, gpa
   from major m
   join student s
   on s.sat = m.minsat
-  order by MinSAT desc
+  order by MinSAT desc*/
  -- this answer is wrong because I did not use PK and FK to join - data is going to be wrong
  
+ select * from student s
+  join major m
+  on m.id = s.majorid
+  -- this is correctly joined and gives right answer
