411144b8e4df6bc3b7c15c581b86fc7da771601c
Eileen Byrne <byrne.me@gmail.com>
2020-09-22 12:06:10 -0400

    update transaction query

--- Transaction sample on PRS.sql
+++ Transaction sample on PRS.sql
@@ -1,2 +1,9 @@
 -- select * from user
-;
+Start Transaction; -- can also use BEGIN (without word Transaction)
+
+Insert User (Username, Password, FirstName, LastName, PhoneNumber, Email)
+  Values ('xxxx', 'xxxx', 'xxxx', 'xxxx', 'xxxx', 'xxxx');
+  
+ select * from User; 
+
+Rollback;
